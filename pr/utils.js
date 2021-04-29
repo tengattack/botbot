a25eeefea385f2427359d844d21afedd91eab04f
Aoko <aokodesuka@gmail.com>
2021-04-29 16:49:17 +0800

    bug fix

--- pr/utils.js
+++ pr/utils.js
@@ -24,8 +24,8 @@ export const initClient = async () => {
   return { github, user }
 }
 
-export const beforeCreatePR = async (project) => {
-  const repoPath = getRepoPath(project)
+export const beforeCreatePR = async (repoName) => {
+  const repoPath = getRepoPath(repoName)
   const branchName = `PR-${day().format('YYYY-MM-DD')}`
   await spawnAsync('git', ['fetch', 'upstream'], { cwd: repoPath })
   await spawnAsync('git', ['checkout', '-f', '-b', branchName, 'upstream/master'], {
@@ -37,12 +37,13 @@ export const beforeCreatePR = async (project) => {
 
 export const beforeUpdatePR = async (project, id) => {
   const { github, user } = await initClient()
+  const repoName = getRepoName(project)
+  const repoPath = getRepoPath(repoName)
   console.log('检查 PR 中……')
   const pull = await github.getPullRequest(project, id)
   if (!pull) {
     throw new Error(`未找到 ${user.login} 为仓库 ${repoName} 创建的 PR (#${id})`)
   }
-  const repoPath = getRepoPath(project)
   // 要求 repo 里的 branchName(pull request head ref) 和本地的相同，且本地存在该分支
   // 正常操作是没有问题的
   await spawnAsync('git', ['checkout', '-f', pull.head.ref], { cwd: repoPath })
